3e3c1193300a34750b5e184770a7391832f25722
Andriantomanga <nabil.arrowbase@gmail.com>
2017-06-27 01:18:29 +0200

    Example using Spark SQL and Hive. Loading data from files stored in HDFS to Hive table

---
+++ spark-hive-samples/src/main/java/org/apache/spark/hive/samples/SparkFromPlainTextToHive.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to JeryTodik under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. JeryTodik licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.spark.hive.samples;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.hive.samples.common.Employee;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * @author nabil
+ */
+public class SparkFromPlainTextToHive {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Creating a Spark session ...
+
+		SparkSession sparkSession = SparkSession.builder().appName("org.apache.spark.nabilexamples").master("local")
+				.config("spark.cores.max", "10").enableHiveSupport().getOrCreate();
+
+		// Creating RDD of formatted data ...
+
+		JavaRDD<Employee> employeeRDD = sparkSession.sparkContext()
+				.textFile("hdfs://localhost:8020/user/nabil/employee.txt", 1).toJavaRDD()
+				.map(new Function<String, Employee>() {
+
+					public Employee call(String line) throws Exception {
+
+						final String[] data = line.split(",");
+						return new Employee(data[0].trim(), Integer.valueOf(data[1].trim()), data[2].trim());
+					}
+
+				});
+
+		// Converting RDD to DataSet ...
+		
+		Dataset<Row> employeeDS = sparkSession.createDataFrame(employeeRDD, Employee.class);
+
+		// Creating temp view for queries ...
+
+		employeeDS.createOrReplaceTempView("employee_view");
+
+		// Querying ...
+
+		Dataset<Row> youngPeople = sparkSession.sql("SELECT name, age FROM employee_view WHERE age BETWEEN 18 AND 30");
+
+		// Show the results of previous query ...
+		
+		youngPeople.show();
+
+		// Storing results in Hive ...
+		
+		sparkSession
+				.sql("CREATE TABLE IF NOT EXISTS employee (name String, age int) COMMENT 'Employee between 18 and 30'");
+
+		sparkSession.sql("INSERT INTO TABLE employee SELECT name, age from employee_view");
+
+		sparkSession.close();
+	}
+
+}
---
+++ spark-hive-samples/src/main/java/org/apache/spark/hive/samples/common/Employee.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to JeryTodik under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. JeryTodik licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.spark.hive.samples.common;
+
+/**
+ * @author nabil
+ */
+public class Employee {
+
+	private String name;
+	
+	private int age;
+	
+	private String company;
+	
+	public Employee() {
+		super();
+	}
+
+	/**
+	 * @param name
+	 * @param age
+	 * @param company
+	 */
+	public Employee(String name, int age, String company) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.company = company;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the company
+	 */
+	public String getCompany() {
+		return company;
+	}
+
+	/**
+	 * @param company the company to set
+	 */
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	
+}
